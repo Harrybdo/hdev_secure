fbf41fdb37022a8e0cbfa407545035ef22af7f05
Harrison Bryant <harrison@brain.do>
2017-02-06 14:55:04 -0500

    gtm

--- html/wp-content/themes/bones/header.php
+++ html/wp-content/themes/bones/header.php
@@ -35,9 +35,13 @@
 		<?php wp_head(); ?>
 		<?php // end of wordpress head ?>
 
-		<?php // drop Google Analytics Here ?>
-		<?php // end analytics ?>
-
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5QVLCT');</script>
+    <!-- End Google Tag Manager -->
 	</head>
 
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
